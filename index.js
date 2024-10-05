d5800352c8e5ae2fca4ef6316da6c01fd844d714
ypranab11 <ypranab11@gmail.com>
2024-10-05 19:40:43 +0600

    api added

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const express = require("express");
 const cors = require("cors");
 
-const courses = require("./courseDetails.json");
+const courses = require("./data/courseDetails.json");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-
 app.get("/", (req, res) => {
+  res.send("Server is Running.");
+});
+app.get("/api/products", (req, res) => {
   res.send(courses);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourse = courses.find((course) => course._id == id);
+  res.send(selectedCourse);
+});
+
+app.get("/category/:id", (req, res) => {
+  const course_id = req.params.id;
+  const selectedCategory = courses.filter(
+    (course) => course.course._id === course_id
+  );
+  res.send(selectedCategory);
 });
 
 app.listen(port, () => {
